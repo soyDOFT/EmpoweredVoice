6129853d7b63a56cfdb44a11b6ef45d96907e95f
mtorrealbagarcia@gmail.com <mtorrealbagarcia@gmail.com>
2024-08-30 13:27:21 -0400

    added hero image to homepage

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
+        'hero': "url('/2.svg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
