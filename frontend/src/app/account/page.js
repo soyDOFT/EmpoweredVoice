1c7a969ef9463e118d0135a0155c68808d7af083
David <soydoft@gmail.com>
2024-08-29 23:36:54 -0400

    add authentication, add logging in, add logging out

---
+++ frontend/src/app/account/page.js
@@ -0,0 +1,10 @@
+import GoogleLogoutButton from '@/components/GoogleLogoutButton'
+
+export default function Page() {
+    return (
+        <div>
+            <p>Account</p>
+            <GoogleLogoutButton/>
+        </div>
+    )
+}
