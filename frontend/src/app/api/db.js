329be98dfcd997f064fb2ab1ba430313b5f76e9f
David <soydoft@gmail.com>
2024-09-04 19:47:29 -0400

    Fix countdown timer to election

--- frontend/src/app/api/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2';
-
-export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
-});
--- frontend/src/components/Countdown.js
+++ frontend/src/components/Countdown.js
@@ -2,48 +2,64 @@
 import React, { useState, useEffect,useRef} from 'react';
 
 export default function Countdown() {
-    const [counter, setCounter] = useState(99);  
+    const [counter, setCounter] = useState({days: 0, hours: 0, minutes: 0, seconds: 0});
     // Update countdown value
     const countdownRef = useRef(null);
+    const electionDate = new Date("2024-11-05T00:00:00");
 
     useEffect(() => {
+      function calcCounter() {
+        const now = new Date();
+        const difference = electionDate - now;
+        console.log(difference);
+
+        if (difference > 0) {
+          const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+          const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+          const minutes = Math.floor((difference / 1000 / 60) % 60);
+          const seconds = Math.floor((difference / 1000) % 60);
+
+          setCounter({days, hours, minutes, seconds});
+        } else {
+          setCounter({days: 0, hours: 0, minutes: 0, seconds: 0})
+        }
+      }
+
         if (countdownRef.current) {
           const validValue = Math.max(0, Math.min(99, counter));
           countdownRef.current.style.setProperty('--value', validValue);
         }
-      }, [counter]);
-    
-      useEffect(() => {
+
         const interval = setInterval(() => {
-          setCounter(prevCounter => (prevCounter > 0 ? prevCounter - 1 : 0));
+          calcCounter();
         }, 1000);
     
         return () => clearInterval(interval);
-      }, []);
+      }, [electionDate]);
 
   return (
     <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
     <div className="flex flex-col">
       <span className="countdown font-mono text-5xl">
-        <span style={{ "--value": 61 }}></span>
+        <span style={{ "--value": counter.days }}></span>
       </span>
       days
     </div>
     <div className="flex flex-col">
       <span className="countdown font-mono text-5xl">
-        <span style={{ "--value": 21 }}></span>
+        <span style={{ "--value": counter.hours }}></span>
       </span>
       hours
     </div>
     <div className="flex flex-col">
       <span className="countdown font-mono text-5xl">
-        <span style={{ "--value": 24 }}></span>
+        <span style={{ "--value": counter.minutes }}></span>
       </span>
       min
     </div>
     <div className="flex flex-col">
       <span className="countdown font-mono text-5xl" ref={countdownRef}>
-        <span></span>
+        <span style={{ "--value": counter.seconds }}>{counter.seconds}</span>
       </span>
       sec
     </div>
