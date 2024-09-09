7e15d0bf3ac9537c28f53d0ba4a295e6ba0255e2
cperalt <145234956+cperalt@users.noreply.github.com>
2024-09-09 14:51:19 -0400

    Added required attribute to inputs

--- frontend/src/app/elections/page.js
+++ frontend/src/app/elections/page.js
@@ -101,6 +101,7 @@ export default function Page() {
                                 type="text"
                                 placeholder="1234 Street Address Rd, City, State"
                                 className="w-full pr-20 text-lg border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                                required
                             />
                             <button 
                                 type="submit"
